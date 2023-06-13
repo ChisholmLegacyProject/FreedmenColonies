58ca313bf21b66fbc0858c5aef89c442228a1902
ChisholmLegacyProject <135271749+ChisholmLegacyProject@users.noreply.github.com>
2023-06-13 14:47:07 -0400

    Update dashmap.py

--- dashmap.py
+++ dashmap.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import geopandas as gpd
 import requests
+import folium
+from streamlit_folium import folium_static
 
 url = 'https://raw.githubusercontent.com/ChisholmLegacyProject/FreedmenColonies/main/FC.geojson'
 response = requests.get(url)
@@ -20,7 +22,43 @@ def main():
 
     # Display map
     st.subheader('Map')
-    st.map(geodata, zoom=2, tooltip='Prime Name')
+    m = folium.Map(location=[0, 0], zoom_start=2)
+
+    for feature in data['features']:
+        properties = feature['properties']
+        name = properties['name']
+        total_population = properties['total_popu']
+        male_population = properties['male_popul']
+        female_population = properties['female_pop']
+        black_population = properties['black']
+        latino_population = properties['latino']
+        poverty = properties['poverty']
+        prime_name = properties['Prime_name']
+        other_name = properties['Other_name']
+        risk_score = properties['RISK_SCORE']
+        sovi_score = properties['SOVI_SCORE']
+
+        popup_text = f'''
+            Name: {name}<br>
+            Total Population: {total_population}<br>
+            Male Population: {male_population}<br>
+            Female Population: {female_population}<br>
+            Black Population: {black_population}<br>
+            Latino Population: {latino_population}<br>
+            Poverty: {poverty}<br>
+            Prime Name: {prime_name}<br>
+            Other Name: {other_name}<br>
+            Risk Score: {risk_score}<br>
+            SOVI Score: {sovi_score}
+        '''
+
+        folium.Marker(
+            location=(feature['geometry']['coordinates'][1], feature['geometry']['coordinates'][0]),
+            popup=folium.Popup(popup_text, max_width=250),
+            icon=folium.Icon(icon='info-sign')
+        ).add_to(m)
+
+    folium_static(m)
 
     # Display individual variables
     st.subheader('Individual Variables')
@@ -43,13 +81,4 @@ def main():
         st.write(f'Male Population: {male_population}')
         st.write(f'Female Population: {female_population}')
         st.write(f'Black Population: {black_population}')
-        st.write(f'Latino Population: {latino_population}')
-        st.write(f'Poverty: {poverty}')
-        st.write(f'Prime Name: {prime_name}')
-        st.write(f'Other Name: {other_name}')
-        st.write(f'Risk Score: {risk_score}')
-        st.write(f'SOVI Score: {sovi_score}')
-        st.write('---')
-
-if __name__ == '__main__':
-    main()
+        st.write(f'Lat
